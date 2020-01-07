fa8537a77778ae372e2ed6e5a21a94a48952613a
anam soomro <anamsoomroed@gmail.com>
2020-01-07 03:28:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -49,9 +49,15 @@ def reduce_to_all_true(source_array)
   result = true
   i = 0
   while i < source_array.length do 
-    
+    compare = source_array[i]
+    if compare && result
+      result = true
+    else
+      result = false
+    end
     i += 1
   end
+  result
 end
 
 
